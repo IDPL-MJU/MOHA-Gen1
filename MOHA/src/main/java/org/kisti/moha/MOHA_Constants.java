08d44ec27785b6eadd8b229e4f3a7d34c048ee4b
Jik-Soo Kim <jiksoo75@kisti.re.kr>
2016-09-12 17:30:51 +0900

    rebuild

--- MOHA/src/main/java/org/kisti/moha/MOHA_Constants.java
+++ MOHA/src/main/java/org/kisti/moha/MOHA_Constants.java
@@ -8,7 +8,7 @@ public interface MOHA_Constants {
 	
 	public static final String JDL_NAME = "INPUT.jdl"; //Fix the name of JDL file to be stored in HDFS
 	
-	/* The default number of retrials to check whether the queue is empty or not */
+	/* The default number of retrying to check whether the queue is empty or not */
 	public static final String DEFAULT_EXECUTOR_RETRIALS = "3"; 
 		
 	public static final int ACTIVEMQ_PRODUCER = 0;
